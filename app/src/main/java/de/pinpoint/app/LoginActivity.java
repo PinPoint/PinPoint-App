1e63fd0b4bc0f90a1961c58ae1975c5ba955fc0b
Nico Franke <nico.franke01@gmail.com>
2020-10-29 17:50:19 +0100

    :construction: Implement initial settings page

--- app/src/main/java/de/pinpoint/app/LoginActivity.java
+++ app/src/main/java/de/pinpoint/app/LoginActivity.java
@@ -1,8 +1,13 @@
 package de.pinpoint.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button button = findViewById(R.id.nextButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
