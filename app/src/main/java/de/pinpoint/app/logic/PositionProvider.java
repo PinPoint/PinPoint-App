609399cf0fab66610f883b751f0206930efab266
Thorben0 <thorben.stumpe@gmail.com>
2021-01-09 21:10:14 +0100

    :bug: Fix GPS crashes and delay

--- app/src/main/java/de/pinpoint/app/logic/PositionProvider.java
+++ app/src/main/java/de/pinpoint/app/logic/PositionProvider.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
@@ -25,6 +26,7 @@ public class PositionProvider implements LocationListener {
         try {
             LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
             manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
             gpsUpdatesRequested = true;
         } catch(SecurityException ex){
             System.out.println("GPS Permission not given yet....");
@@ -46,5 +48,20 @@ public class PositionProvider implements LocationListener {
     public void onLocationChanged(@NonNull Location location) {
         System.out.println("location listener updated");
         this.lastLocation = location;
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        this.lastLocation = null;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 }
