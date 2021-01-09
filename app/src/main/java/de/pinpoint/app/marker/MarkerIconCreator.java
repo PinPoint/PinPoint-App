c04b4ba8d38d410109d31668eaa7150c6534eac4
Thorben0 <thorben.stumpe@gmail.com>
2021-01-09 18:33:53 +0100

    :bug: Properly draw Marker Icon

--- app/src/main/java/de/pinpoint/app/marker/MarkerIconCreator.java
+++ app/src/main/java/de/pinpoint/app/marker/MarkerIconCreator.java
@@ -17,7 +17,7 @@ import de.pinpoint.app.R;
 public class MarkerIconCreator {
     private String name;
     private String color = "#f44336";
-    private int size = 150;
+    private int size = 125;
 
     public BitmapDrawable createIcon(Context context) {
         Paint textPaint = new Paint();
@@ -27,24 +27,41 @@ public class MarkerIconCreator {
         textPaint.setTypeface(Typeface.DEFAULT_BOLD);
         textPaint.setTextAlign(Paint.Align.LEFT);
 
-        final int width = (int) (textPaint.measureText(name) + 0.5f);
+        /* Bounds of the text */
+        int textHeight = 30;
+        int textWidth = (int) textPaint.measureText(name);
 
+        /* Bounds of the marker image */
+        int imageHeight = size;
+        int imageWidth = size;
+        int imageX = Math.max(0, (textWidth - imageWidth) / 2);
 
+        /* Calculate Bounds of the total image*/
+        int totalHeight = textHeight + imageHeight;
+        int totalWidth = Math.max(imageWidth, textWidth);
+
+        /* Create icon Bitmap Canvas */
+        Bitmap bitmap = Bitmap.createBitmap(totalWidth, totalHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        /* Draw colored marker image on Canvas */
+        Drawable coloredImage = this.getColoredMarker(context);
+        coloredImage.setBounds(imageX, textHeight, imageX + size, size + textHeight);
+        coloredImage.draw(canvas);
+
+        /* Draw text on canvas*/
+        int textX = Math.max(0, (imageWidth - textWidth) / 2);
+        canvas.drawText(name, textX, textHeight, textPaint);
+
+        /* Return Bitmap as BitMap Drawable*/
+        return new BitmapDrawable(context.getResources(), bitmap);
+    }
+
+    private Drawable getColoredMarker(Context context) {
         Drawable unwrappedDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_person_pin);
         Drawable wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable);
         DrawableCompat.setTint(wrappedDrawable, Color.parseColor(color));
-
-
-        wrappedDrawable.setBounds(0, 20, size-20, size);
-
-        Bitmap image = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
-
-        Canvas canvas = new Canvas(image);
-        wrappedDrawable.draw(canvas);
-        canvas.drawText(name, 0, 30, textPaint);
-
-        Paint markerPaint = new Paint();
-        return new BitmapDrawable(context.getResources(), image);
+        return wrappedDrawable;
     }
 
     public void setName(String name) {
